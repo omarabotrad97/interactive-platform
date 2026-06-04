e68eed47e4f5c22e6179a02dc486436acad03dc9
OmarAboTrad <62908732+OmarAboTrad@users.noreply.github.com>
2026-06-04 14:48:11 +0300

    Add GitHub Actions workflow and configure base path for deploy

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/interactive-platform/',
     plugins: [react()],
     resolve: {
         alias: {
