315b4c471548e37c526873fcc13381bbca5a46c9
OmarAboTrad <62908732+OmarAboTrad@users.noreply.github.com>
2026-06-10 20:32:48 +0300

    Configure api baseURL to point to Render cloud server

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://wisdom-house-api.onrender.com/api',
 });
 
 // Add a request interceptor to add the JWT token to headers
